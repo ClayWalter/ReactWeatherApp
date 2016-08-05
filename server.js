982eec259d3b2c10a6ef8b58a61a8fc31344d2fe
clayhero <theclayhero@gmail.com>
2016-08-05 19:46:26 -0400

    Fixed syntax error on server.js

--- server.js
+++ server.js
@@ -4,12 +4,11 @@ var express = require('express');
 var app = express();
 const PORT = process.env.PORT || 3000;
 
-
 app.use(function (req, res, next){
   if (req.headers['x-forwarded-proto'] === 'http') {
     next();
   } else {
-    res.redirect('http://') + req.hostname + req.url);
+    res.redirect('http://' + req.hostname + req.url);
   }
 });
 
