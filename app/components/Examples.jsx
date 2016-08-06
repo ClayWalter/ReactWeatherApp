ba7d217e99814f7ea9822c320516bfdd3da059ee
clayhero <theclayhero@gmail.com>
2016-08-06 11:56:05 -0400

    Add grid system and initial style for examples

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,19 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) => {
     return (
     <div>
-      <h3>Examples</h3>
-      <p>Welcome to examples page!</p>
+      <h1 className="text-center">Examples</h1>
+      <p>Here are a few example locations to test out the app:</p>
+        <ol>
+          <li>
+            <Link to='/?location=Kingston'>Kingston, NY</Link>
+          </li>
+          <li>
+            <Link to='/?location=Austin'>Austin, TX</Link>
+          </li>
+        </ol>
     </div>
     )
   };
