3157ebe967921cba47f75e49308c1b07cb2fd583
clayhero <theclayhero@gmail.com>
2016-08-05 20:00:54 -0400

    Added content to About Page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,7 +2,10 @@ var React = require('react');
 
 var About = (props) => {
   return (
-    <h3>About Component</h3>
+  <div>
+    <h1>About</h1>
+    <p> Welcome to the about page sucka.</p>
+  </div>
     )
 };
 
