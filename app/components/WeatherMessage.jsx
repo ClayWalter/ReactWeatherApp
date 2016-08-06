ccd2db2e1169869435fd897f4889f2a9e67c7f42
clayhero <theclayhero@gmail.com>
2016-08-06 12:55:17 -0400

    Add initial style to Weather Page

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -2,7 +2,7 @@ var React = require('react');
 
 var WeatherMessage = ({temp, location}) => {
   return (
-    <h3>It is it {temp} in {location}.</h3>
+    <h3 className="text-center">It is it {temp} in {location}.</h3>
   )
 }
 module.exports = WeatherMessage;
