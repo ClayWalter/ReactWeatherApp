7d33efe2298277dff3de0f359aa3527034c99107
clayhero <theclayhero@gmail.com>
2016-08-07 11:07:19 -0400

    Converted to scss

--- app/app.jsx
+++ app/app.jsx
@@ -12,7 +12,7 @@ require('style!css!foundation-sites/dist/foundation.min.css'),
 $(document).foundation();
 
 // APP css
-require('style!css!applicationStyles')
+require('style!css!sass!applicationStyles')
 
 
 ReactDOM.render(
